3a9f5afda13dd9ecbdbc1d868b44a70712ed7eea
Diego Carballeda <39389013+diegocarba99@users.noreply.github.com>
2020-12-30 22:43:57 +0100

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -9,16 +9,15 @@ var name = 'Diego';
 var gree1 = 'Jun lotara! ';
 var gree2 = 'Egunon! ';
 var gree3 = 'Arratsaldeon ';
-var gree4 = 'Arratsaldeon ';
-var gree5 = 'Arratsaldeon ';
-var gree6 = 'Arratsaldeon ';
+var gree4 = 'Gabon ';
+
 
 // Define the hours of the greetings
 if (hour >= 23 && hour < 5) {
     document.getElementById('greetings').innerText = gree1 + name;
 } else if (hour >= 6 && hour < 12) {
     document.getElementById('greetings').innerText = gree2 + name;
-} else if (hour >= 12 && hour < 17) {
+} else if (hour >= 12 && hour < 20) {
     document.getElementById('greetings').innerText = gree3 + name;
 } else  {
     document.getElementById('greetings').innerText = gree4 + name;
