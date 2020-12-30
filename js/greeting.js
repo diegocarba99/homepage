3eca6082a3d0d2cc8f8f25bce958f5d52f7fc8f6
diegocarba99 <diegocarballedamartinez@gmail.com>
2020-12-30 22:41:27 +0100

    Added folders

---
+++ js/greeting.js
@@ -0,0 +1,25 @@
+// Get the hour
+var today = new Date();
+var hour = today.getHours();
+
+// Here you can change your name
+var name = 'Diego';
+
+// Here you can change your greetings
+var gree1 = 'Jun lotara! ';
+var gree2 = 'Egunon! ';
+var gree3 = 'Arratsaldeon ';
+var gree4 = 'Arratsaldeon ';
+var gree5 = 'Arratsaldeon ';
+var gree6 = 'Arratsaldeon ';
+
+// Define the hours of the greetings
+if (hour >= 23 && hour < 5) {
+    document.getElementById('greetings').innerText = gree1 + name;
+} else if (hour >= 6 && hour < 12) {
+    document.getElementById('greetings').innerText = gree2 + name;
+} else if (hour >= 12 && hour < 17) {
+    document.getElementById('greetings').innerText = gree3 + name;
+} else  {
+    document.getElementById('greetings').innerText = gree4 + name;
+}
